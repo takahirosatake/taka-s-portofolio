5d0cf7700ae34312b901efbf56fb1ab74c2b255b
ponsata <khr.04237@gmail.com>
2021-05-27 01:30:49 +0900

    config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/taka_portfolio/'
   : '/',
+  outputDir: 'docs',
   css:{ 
     loaderOptions:{
         scss:{additionalData: '@import "@/assets/scss/main.scss";' 
