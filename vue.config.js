817762dca488e66384f8485f93833a48b9302d6a
ponsata <khr.04237@gmail.com>
2021-05-28 14:40:30 +0900

    contactFunction

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/'
@@ -8,5 +11,21 @@ module.exports = {
         scss:{additionalData: '@import "@/assets/scss/main.scss";' 
       }
     },
+  },
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          [ '/'],
+          {
+            // options
+          }
+        ),
+      ]
+    }
   }
 }
