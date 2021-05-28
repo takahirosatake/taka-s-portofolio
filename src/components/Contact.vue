53a12a02d99b7d5f19e6e41551dc8f0fac947b23
ponsata <khr.04237@gmail.com>
2021-05-28 16:45:27 +0900

    contactFunction2

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,113 +1,126 @@
 <template>
   <section class="contact-container">
-    <template v-if="!isSubmit">
-        <form name="contact" class="contact__form" @submit.prevent="onSubmit" method="POST" data-netlify="true" :class="sendingClass">
-        <div class="contact__item">
-        <label for="username">お名前</label>
-      <input type="text" id="username" name="username"  v-model="contact.username" autocomplete="name">
-      </div>
-      <p v-show="hasError.username" class="error">氏名は20文字以内で入力してください。</p>
-      <!-- computed で監視している値 -->
-      <!-- <p :class="{error: hasError.username}"> {{ contact.username.length }} / 20</p> -->
-      <div class="contact__item">
-        <label for="email">メールアドレス</label>
-        <input type="email" v-model.lazy.trim="contact.email" id="email" name="email">
-      </div>
-      <div class="contact__item">
-        <label for="message">メッセージ</label>
-        <textarea id="message" name="message"  v-model="contact.message"></textarea>
-      </div>
-      <div class="contact__submit">
-        <input type="submit" value="送信">
-      </div>
-      </form>
+    <template v-if="!finished">
+        <div class="contact__error" v-if="errors.length">
+          <ul class="error" v-for="error in errors" :key="error.id">
+            <li>{{ error }}</li>
+          </ul>
+        </div>
+        <form 
+          name="contact" 
+          class="contact__form" 
+          @submit.prevent="handleSubmit" 
+          method="POST" 
+          data-netlify="true">
+        
+          <div class="contact__item">
+            <label for="username">お名前</label>
+            <input 
+              type="text" 
+              id="username" 
+              name="username"  
+              v-model="username" 
+              autocomplete="name">
+          </div>
+           <!-- <p :class="{error: hasError.yourName}"> {{ contact.username.length }} / 20</p> -->
+           <p v-show="hasError.username" class="error">氏名は20文字以内で入力してください。</p>
+          <div class="contact__item">
+            <label for="email">メールアドレス</label>
+            <input 
+              type="email" 
+              v-model.lazy.trim="contact.email" 
+              id="email" 
+              name="email">
+          </div>
+          <div class="contact__item">
+            <label for="message">メッセージ</label>
+            <textarea 
+              id="message" 
+              name="message"  
+              v-model="contact.message" >
+            </textarea>
+          </div>
+          <div class="contact__submit">
+            <input type="submit" value="送信">
+          </div>
+        </form>
     </template>
     <template v-else>
-      <p v-text="'お問い合わせ頂きありがとうございました。'" />
-      <p><router-link to="/" v-text="'ホームへ'" /></p>
+      <div v-text="'お問い合わせ頂きありがとうございました。'" />
+      <div><router-link to="/" v-text="'ホームへ'" /></div>
     </template>
   </section>
 </template>
 <script>
 import axios from 'axios'
-export default {  
-      name:'',
-      data(){
-        return {
-          contact: {
-            username: '',
-            email:'',
-            message:'',
-            isSubmit        : false,
-            isSending       : false,
-            isError         : false,
-            completeMessage : '',
-          },
-          errors: [],
-          hasError:{
-            username: false
-          }
-        }
+export default {
+  data() {
+    return {
+      contact: {
+          username: '',
+          email:'',
+          message:'',
       },
-      methods:{
-        onSubmit(){
-          if(this.isSending){
-            return;
-          }
-          this.isSending = true;
-          this.completeMessage = '送信処理中…';
-          
-          this.errors = []
-          const params = new URLSearchParams();
-          params.append('form-name', 'contact');
+      errors: [],
+      hasError:{
+          username: false,
+          email: false
+      },
+      finished: false
+    }
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&')
+    },
+    handleSubmit() {
 
-          if(!this.contact.username){
-            this.errors.push('氏名は必須です')
-          } else if(this.contact.username.length > 20){
-            this.errors.push('氏名は20文字以内で入力してください')
-          }
-          if(!this.contact.email){
-            this.errors.push('メールアドレスは、必須です')
-          }
-          if(!this.contact.message){
-            this.errors.push('メッセージは、必須です')
-          }
-            params.append('username', this.username);
-            params.append('email', this.email);
-            params.append('message', this.message);
-          
-          axios
-          .$post('/', params)
-          .then(() => {
-            this.completeMessage = 'お問い合わせを送信しました！';
-            this.resetForm();
-            this.isSubmit  = true;
-          })
-          .catch(err => {
-            this.completeMessage = 'お問い合わせの送信が失敗しました';
-            this.isError = true;
-          })
-          .finally(() => {
-            this.isSending = false;
-          });
-        },
-        resetForm() {
+      this.errors = []
+      if(!this.contact.username){
+          this.errors.push('氏名は必須です')
+      } else if(this.contact.username.length > 20){
+          this.errors.push('氏名は20文字以内で入力してください')
+      }
+      if(!this.contact.email){
+        this.errors.push('メールアドレスは、必須です')
+      }
+      if(!this.contact.message){
+        this.errors.push('メッセージは、必須です')
+      }
+
+      const axiosConfig = {
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+      axios
+        .post(
+          '/',
+          this.encode({
+            'form-name': 'contact',
+            ...this.contact
+          }),
+          axiosConfig
+        )
+        .then(() => {
+          this.finished = true
+          this.resetForm();
+        })
+        .catch(error => {
+          console.log(error.response)
+        });
+    },
+    resetForm() {
           this.username        = '';
           this.email           = '';
           this.message         = '';
           this.errors          = [];
-          this.isError         = false;
           return
-        },
-      },
-      computed:{
-        sendingClass() {
-          return {
-            'is-sending' : this.isSending,
-            'is-error' : this.isError,
-            'is-complete' : this.isSubmit,
-          }
-        },
+    },
+  },
+  computed:{
         username:{
           get : function(){
             return this.contact.username
@@ -129,5 +142,8 @@ export default {
 .contact-container {
   padding: 64px;
   text-align: center;
+}
+.error {
+  color:red
 }
 </style>
